d85f86d4b45ad50aef34fba854eee3345efcd6f2
Kevin Zuern <kevin@kevinzuern.com>
2016-12-03 14:43:56 -0500

    Add base for details page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/details/:id', function(req, res) {
+  var id = req.params["id"];
+  // do stuff
+  res.render('details', { title: "Details" });
+});
+
 module.exports = router;
