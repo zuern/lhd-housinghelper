286a0eae35c1100220379fd585b7db022f0d8e70
Brendan Kolisnik <bkolisnik@gmail.com>
2016-12-03 19:11:45 -0500

    Random data added

--- services/dbService.js
+++ services/dbService.js
@@ -9,7 +9,7 @@ define(function (require, exports, module) {
 
 });
 
-
+var faker = require('faker');
 var MongoClient = require('mongodb').MongoClient;
 var MONGOURL = 'mongodb://localhost:27017/db';
 var db;
@@ -31,43 +31,124 @@ function populateDatabase(){
     return;
   }
 
-  var collection = db.collection('houses');
-  collection.insert({
-    title: 'house1',
-    thumbnail: 'http://placehold.it/150x150?text=thumb',
-    images: [
-    'http://placehold.it/800x600?text=1',
-    'http://placehold.it/800x600?text=2',
-    'http://placehold.it/800x600?text=3'
-    ],
-    shortDescription: 'This is an awesome unit within walking distance to Queen\'s',
-    longDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    price: 500,
-    priceInterval: 'Monthly',
-    leaseType: 'Annual',
-    landlordName: 'Jim Jom',
-    latitude: '44.228646',
-    longitude: '-76.502897',
-    laundry: 'On Site',
-    includedUtilities: [
-    'Heat',
-    'Hydro'
-    ],
-    isFurnished: true,
-    numBedrooms: 3,
-    numBathrooms: 2,
-    hasCarParking: false,
-    hasBikeParking: true,
-    hasInternetIncluded: true,
-  });
+var collection = db.collection('houses');
+var data1 = {
+        title: faker.name.findName(),
+        thumbnail: faker.image.image(),
+        images: [faker.image.image(),faker.image.image(),faker.image.image()],
+        shortDescription: faker.lorem.words(),
+        longDescription: faker.lorem.words(),
+        price: faker.finance.amount(),
+        priceInterval: faker.lorem.words(),
+        leaseType: faker.lorem.words(),
+        landLordName: faker.name.findName(),
+        latitude: String(44.228646 + (Math.random()*0.0005)),
+        longitude: String(-76.502897 + (Math.random()*0.000.5)),
+        laundry: faker.lorem.words(),
+        includedUtilities: [faker.lorem.words(),faker.lorem.words()],
+        isFurnished: faker.lorem.words(),
+        numBedrooms: faker.random.number(),
+        numBathrooms: faker.random.number(),
+        hasCarParking: faker.lorem.words(),
+        hasBikeParking: faker.lorem.words(),
+        hasInternetIncluded: faker.lorem.words()
+      }
+var data2 = {
+        title: faker.name.findName(),
+        thumbnail: faker.image.image(),
+        images: [faker.image.image(),faker.image.image(),faker.image.image()],
+        shortDescription: faker.lorem.words(),
+        longDescription: faker.lorem.words(),
+        price: faker.finance.amount(),
+        priceInterval: faker.lorem.words(),
+        leaseType: faker.lorem.words(),
+        landLordName: faker.name.findName(),
+        latitude: String(44.228646 + (Math.random()*0.0005)),
+        longitude: String(-76.502897 + (Math.random()*0.0005)),
+        laundry: faker.lorem.words(),
+        includedUtilities: [faker.lorem.words(),faker.lorem.words()],
+        isFurnished: faker.lorem.words(),
+        numBedrooms: faker.random.number(),
+        numBathrooms: faker.random.number(),
+        hasCarParking: faker.lorem.words(),
+        hasBikeParking: faker.lorem.words(),
+        hasInternetIncluded: faker.lorem.words()
+      }
+var data3 = {
+        title: faker.name.findName(),
+        thumbnail: faker.image.image(),
+        images: [faker.image.image(),faker.image.image(),faker.image.image()],
+        shortDescription: faker.lorem.words(),
+        longDescription: faker.lorem.words(),
+        price: faker.finance.amount(),
+        priceInterval: faker.lorem.words(),
+        leaseType: faker.lorem.words(),
+        landLordName: faker.name.findName(),
+        latitude: String(44.228646 + (Math.random()*0.0005)),
+        longitude: String(-76.502897 + (Math.random()*0.0.0005)),
+        laundry: faker.lorem.words(),
+        includedUtilities: [faker.lorem.words(),faker.lorem.words()],
+        isFurnished: faker.lorem.words(),
+        numBedrooms: faker.random.number(),
+        numBathrooms: faker.random.number(),
+        hasCarParking: faker.lorem.words(),
+        hasBikeParking: faker.lorem.words(),
+        hasInternetIncluded: faker.lorem.words()
+      }
+var data4 = {
+        title: faker.name.findName(),
+        thumbnail: faker.image.image(),
+        images: [faker.image.image(),faker.image.image(),faker.image.image()],
+        shortDescription: faker.lorem.words(),
+        longDescription: faker.lorem.words(),
+        price: faker.finance.amount(),
+        priceInterval: faker.lorem.words(),
+        leaseType: faker.lorem.words(),
+        landLordName: faker.name.findName(),
+        latitude: String(44.228646 + (Math.random()*0.0005)),
+        longitude: String(-76.502897 + (Math.random()*0.0005)),
+        laundry: faker.lorem.words(),
+        includedUtilities: [faker.lorem.words(),faker.lorem.words()],
+        isFurnished: faker.lorem.words(),
+        numBedrooms: faker.random.number(),
+        numBathrooms: faker.random.number(),
+        hasCarParking: faker.lorem.words(),
+        hasBikeParking: faker.lorem.words(),
+        hasInternetIncluded: faker.lorem.words()
+      }
+var data5 = {
+        title: faker.name.findName(),
+        thumbnail: faker.image.image(),
+        images: [faker.image.image(),faker.image.image(),faker.image.image()],
+        shortDescription: faker.lorem.words(),
+        longDescription: faker.lorem.words(),
+        price: faker.finance.amount(),
+        priceInterval: faker.lorem.words(),
+        leaseType: faker.lorem.words(),
+        landLordName: faker.name.findName(),
+        latitude: String(44.228646 - (Math.random()*0.0005)),
+        longitude: String(-76.502897 + (Math.random()*0.0005)),
+        laundry: faker.lorem.words(),
+        includedUtilities: [faker.lorem.words(),faker.lorem.words()],
+        isFurnished: faker.lorem.words(),
+        numBedrooms: faker.random.number(),
+        numBathrooms: faker.random.number(),
+        hasCarParking: faker.lorem.words(),
+        hasBikeParking: faker.lorem.words(),
+        hasInternetIncluded: faker.lorem.words()
+      }
+
+      collection.insert(data1);
+      collection.insert(data2);
+      collection.insert(data3);
+      collection.insert(data4);
+      collection.insert(data5);
+
 
 }
 
-function getCollection(){
-  var collection = db.collection('houses');
 
-  return collection;
-}
+
 
 
 function getHouseList(callback) {
